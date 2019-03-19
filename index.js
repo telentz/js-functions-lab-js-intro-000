5a925332c98796457927daf765cbcfa84c6b8150
Tyler Lentz <lentz.etyler@gmail.com>
2019-03-19 00:09:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // write your code below!
+function happyHolidays(string){
+  return string.happyHolidays("Happy holidays!");
+}
+
+function happyHolidaysTo(name){
+  return `Happy holidays, #{name}!`;
+}
+
+}
+
+function happyHolidaysTo(){
+  return string.happyHolidayTo(`Happy ${holiday}, ${name}!`)
+}
